8fd75dfc0e8ef1f75f6f8f312e59a399569d9343
Jørund Jenssen Berg <jjberg@gmail.com>
2021-12-23 14:39:30 +0100

    Adding sporsmaalsfil to repo

--- test_quiz.py
+++ test_quiz.py
@@ -4,5 +4,21 @@
 Created on Thu Dec 23 14:12:04 2021
 
 @author: jjberg
+
+Skriv en enhetstest for klassen Sporsmaal, som tester i alle fall metodene 
+sjekk_svar og korrekt_svar_tekst
 """
 
+import unittest
+from quiz import MultipleChoice
+
+class TestMultipleChoice(unittest.TestCase):
+    
+    def test_sjekk_svar(self):
+        return True
+        
+    def test_korrekt_svar_tekst(self):
+        return True
+    
+if __name__ == '__main__':
+    unittest.main()
